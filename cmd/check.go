338b3107c43e7d990c0c0e51f60c80cbc97c77f5
Anderson Braz <braz_anderson@msn.com>
2020-05-26 19:41:41 -0300

    Up Files

--- cmd/check.go
+++ cmd/check.go
@@ -13,8 +13,8 @@ var checkCmd = &cobra.Command{
 	Use:   "check",
 	Short: "agent check --service <<name>>",
 	Long: `*****************************************************************
-			This command check enviroment variables for your MongoDB service.
-			*****************************************************************`,
+	This command check enviroment variables for your MongoDB service.
+	*****************************************************************`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		for _, e := range os.Environ() {
